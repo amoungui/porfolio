4c38199b65df081e425068dbf3c229c48f8ebb9c
amoungui <samoungui@gmail.com>
2024-05-20 13:12:22 +0100

    integrate dataprovider in the root file of project

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { DataProvider } from "./contexts/DataContext";
 import Home from './Pages/Home/Home'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
@@ -10,14 +11,16 @@ import Error from './components/Error/Error'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<React.StrictMode>
-		<Router>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="*" element={<Error />} />
-			</Routes>
-			<Footer />
-		</Router>
-	</React.StrictMode>
+	<DataProvider>
+		<React.StrictMode>
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</React.StrictMode>
+	</DataProvider>
 );
