243ae98b8a2113673a7aa67017864cc251526881
amoungui <samoungui@gmail.com>
2024-05-23 12:58:46 +0100

    add aria-label to each link in the Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,16 @@
 import { Link } from 'react-router-dom'
 
 function Footer() {
-
     return (
         <footer className="footer">
-        <p className="footer__title">Amoungui.</p>
-        <div className="footer__social">
-            <Link to="/" className="footer__icon"><i class="bx bxl-facebook"></i></Link>
-            <Link to="/" className="footer__icon"><i class="bx bxl-instagram"></i></Link>
-            <Link to="/" className="footer__icon"><i class="bx bxl-twitter"></i></Link>
-        </div>
-        <p className="footer__copy">&#169; Amoungui. All rigths reserved</p>
-      </footer>
+            <p className="footer__title">Amoungui.</p>
+            <div className="footer__social">
+                <Link to="/" className="footer__icon" aria-label="Facebook"><i class="bx bxl-facebook"></i></Link>
+                <Link to="/" className="footer__icon" aria-label="Instagram"><i class="bx bxl-instagram"></i></Link>
+                <Link to="/" className="footer__icon" aria-label="Twitter"><i class="bx bxl-twitter"></i></Link>
+            </div>
+            <p className="footer__copy">© Amoungui. All rigths reserved</p>
+        </footer>
     )
 }
 
